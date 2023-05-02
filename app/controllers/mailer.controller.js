9beecdc2b20a94c7b8821e749e03aa5a6e2fd946
RickyDj <rickydjunaidy@ymail.com>
2023-05-02 14:24:20 +0700

    feat: nodemailer config

--- app/controllers/mailer.controller.js
+++ app/controllers/mailer.controller.js
@@ -1,5 +1,22 @@
 const TB_CARD = require("../models/tb_card.model.js");
 
+
+// ------------------------- 1 TASK: REFACTOR-------------------------------//
+const nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'notificationnodemailerricky@gmail.com',
+        pass: 'kjbeicxmlqbriohu'
+    }
+});
+
+// ------------------------- 1 TASK: REFACTOR-------------------------------//
+
 exports.get_mail = (req, res) => {
     const fs = require('fs');
 
@@ -17,13 +34,32 @@ exports.send_mail = (req, res) => {
     if (req.body) {
         save_record_to_db(req, res)
         send_email(req.body.email_from, req.body.email_to, greeting_card_html)
+        res.send("Email Berhasil Dikirim")
     }
 
-    //res.send(greeting_card_html)
+    
 };
 
-function send_email(email_from, email_to, template){
-    console.log("SENNNNT!")
+
+function send_email(email_from, email_to, template,){
+
+    // setup email data with unicode symbols
+        let mailOptions = {
+            from: email_from, // sender address
+            to: email_to, // receiver email
+            subject: "Kartu Ucapan dari" + email_from, // Subject line
+            text: "",
+            html: template,
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+        });
+
 }
 
 function save_record_to_db(req, res) {
@@ -36,3 +72,4 @@ function save_record_to_db(req, res) {
     // Save TB_CARD in the database
     TB_CARD.create(tb_card, (err, data) => { console.log("Created new TB Card") });
 }
+
